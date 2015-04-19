d9ca898c93182279a41897ae21bd1bf8473e6456
dany1468 <dany1468@gmail.com>
2015-04-19 20:19:53 +0900

    Refactor spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,11 @@ require 'ohm/set'
 require 'ohm/pop'
 
 RSpec.configure do |config|
-  config.before do
+  config.before :all do
     Ohm.redis = Redic.new("redis://#{ENV['WERCKER_REDIS_HOST'] || 'localhost'}:#{ENV['WERCKER_REDIS_PORT'] || 6379}/")
+  end
+
+  config.before do
     Ohm.flush
   end
 end
