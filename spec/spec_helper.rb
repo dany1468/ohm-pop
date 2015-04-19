c3480bb73ca5c6b9cc3ecfd71d161e3807b76847
dany1468 <dany1468@gmail.com>
2015-04-19 18:57:58 +0900

    Replace Wercker Redis ENV Key

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'ohm/pop'
 
 RSpec.configure do |config|
   config.before do
-    Ohm.flush
     Ohm.redis = Redic.new("redis://#{ENV['WERCKER_REDIS_HOST'] || 'localhost'}:#{ENV['WERCKER_REDIS_PORT'] || 6379}/")
+    Ohm.flush
   end
 end
